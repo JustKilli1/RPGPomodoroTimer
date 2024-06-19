7627717f42eddff20947d5cac64a2e46ceec02fa
JustKilli <80322461+JustKilli1@users.noreply.github.com>
2024-06-19 13:25:53 +0200

    TimeTemplateController added

--- TimeTemplate/build.gradle.kts
+++ TimeTemplate/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "0.1.3-DEV"
+version = "0.1.4-DEV"
 group = "net.justkilli.timetemplate"
---
+++ TimeTemplate/src/main/java/net/justkilli/timetemplate/controller/TimeTemplateController.java
@@ -0,0 +1,11 @@
+package net.justkilli.timetemplate.controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for Creating and Getting TimeTemplates.
+ * */
+@RestController
+@RequestMapping("/api/time_template")
+public class TimeTemplateController {
+
+}
